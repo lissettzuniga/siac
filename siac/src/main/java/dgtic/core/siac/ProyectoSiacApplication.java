00aa76b6424276a1fabcabb9f42d866c14484c12
lissettzuniga <lis.voca11@gmail.com>
2026-02-27 22:22:51 -0600

    Proyecto base SIAC

---
+++ siac/src/main/java/dgtic/core/siac/ProyectoSiacApplication.java
@@ -0,0 +1,13 @@
+package dgtic.core.siac;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoSiacApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoSiacApplication.class, args);
+	}
+
+}
